a79c63fb002464fd2409506036c1d458aa93e0d6
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2022-12-15 13:30:47 +0330

    13. Favicon Color Changer Created.

---
+++ 13. Favicon Color Changer/js/script.js
@@ -0,0 +1,9 @@
+const favicon = document.getElementById('favicon')
+
+function toRed(){
+    favicon.setAttribute('href', 'img/favicon.png')
+}
+
+function toGreen() {
+    favicon.setAttribute('href', 'img/faviconTwo.png')
+}
