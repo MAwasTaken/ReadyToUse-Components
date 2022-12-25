f5235cdc3e4dade2293d3f5675ce13c5a21b990e
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2022-12-25 13:38:00 +0330

    23. Animated Landing Page 2 Created.

---
+++ 23. Animated Landing Page 2/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(src) {
+    document.querySelector('.bmw').src = src
+}
+
+function changeBgColor(color) {
+    const section = document.querySelector('.section')
+    section.style.background = color
+}
