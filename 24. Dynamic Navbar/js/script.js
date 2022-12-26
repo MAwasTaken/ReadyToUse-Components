0824f5b7b2faca997e1ac7a8f9b3ea40fe010c42
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2022-12-26 13:20:35 +0330

    24. Dynamic Navbar Created.

---
+++ 24. Dynamic Navbar/js/script.js
@@ -0,0 +1,13 @@
+let li = document.getElementsByTagName('li')
+
+for (let i = 0; i < li.length; i++) {
+    li[i].onclick = (e) => {
+        let n = 0
+
+        while (n < li.length) {
+            li[n++].className = ''
+        }
+
+        e.target.className = 'active'
+    }
+}
